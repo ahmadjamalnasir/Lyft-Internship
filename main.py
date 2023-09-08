c2fcf4b3b2396101b649dc8b483bc73c2ba3cd40
ahmadjamalnasir <ahmadjamal2k@gmail.com>
2023-09-08 19:35:24 +0500

    restructured classes and improved all code next iteration writing service code

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-class engine:
-    def __init__(self,last_service_miliage, warning_light):
-        self.last_service_miliage = last_service_miliage
-        self.warning_light = warning_light
+class Components:
+    def __init__(self, name: str, service_criteria: int, criteria_type: str):
+        self.name = name
+        self.service_criteria = service_criteria
+        self.criteria_type = criteria_type
 
-class battery:
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+class Engine(Components):
+    def __init__(self, name, service_criteria, criteria_type):
+        # Call the constructor of the parent class (Components)
+        super().__init__(name, service_criteria, criteria_type)
 
-class tires:
-    def __init__(self, last_service_date, last_service_milaige):
-        self.last_service_date = last_service_date
-        self.last_service_milaige = last_service_milaige
+class Battery(Components):
+    def __init__(self, name, service_criteria, criteria_type):
+        # Call the constructor of the parent class (Components)
+        super().__init__(name, service_criteria, criteria_type)
 
-class car(engine, battery, tires):
-    pass
+class Tires(Components):
+    def __init__(self, name, service_criteria, criteria_type):
+        # Call the constructor of the parent class (Components)
+        super().__init__(name, service_criteria, criteria_type)
+
+class Car:
+    def __init__(self,name, engine, battery, tires):
+        self.name = name
+        self.engine = engine
+        self.battery = battery
+        self.tires = tires
+
+# Create instances of the each component class
+capulet_engine = Engine("Capulet Engine", 30000, "miliage")
+willoughby_engine =Engine("Willoughby Engine", 60000, "miliage")
+sternman_engine = Engine("Sternman Engine", 0, "warning light")
+spindler_battery = Battery("Spindler Battery", 2, "time period")
+nubbin_battery = Battery("Nubbin Battery", 4, "time period")
+# Dummy tire instance
+dummy_tire = Tires("Dummy Tires", 50000, "miliage")
+
+# Create instances for car
+calliope  = Car("Calliope", capulet_engine, spindler_battery, dummy_tire)
+glissade = Car("Glissade", willoughby_engine, spindler_battery, dummy_tire)
+palindrome = Car("Palindrome", sternman_engine, spindler_battery, dummy_tire)
+rorschach = Car("Rorschach", willoughby_engine, nubbin_battery, dummy_tire)
+thovex = Car("Thovex", capulet_engine, nubbin_battery, dummy_tire)
